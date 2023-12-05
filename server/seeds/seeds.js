6b0eb7421e29775ef0beaafefcfe4309e20a73f6
Anthony Ciccone <cicconeworldwide@gmail.com>
2023-12-04 21:54:19 -0700

    added some seed data for testing purposes

---
+++ server/seeds/seeds.js
@@ -0,0 +1,29 @@
+const users = [
+    {
+        id: 1,
+        username: "Jimmy",
+        email: "ABC123",
+        password: "Timmy"
+    },
+    {
+        id: 2,
+        username: "Stan",
+        email: "XYZ123",
+        password: "Kyle"
+    }
+];
+
+const cards = [
+    {
+        name: 'Lightning Bolt',
+        type: 'Instant',
+        rarity: 'Common',
+    },
+    {
+        name: 'Dark Confidant',
+        type: 'Creature',
+        rarity: 'Mythic Rare',
+    }
+];
+
+module.exports = users, cards;
