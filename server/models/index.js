25c39bdfd6b5dccb750cbdc55a781a635efb1051
Michael Hannah <Michaelrkhannah@gmail.com>
2023-12-05 19:39:32 -0700

    Fixed layout?

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,7 @@ const cards = require('./CardInfo');
 const user = require('./UserInfo');
 const collections = require('./Collections');
 const profile = require('./Profile');
+const decks = require('./Decks');
 
 
 module.exports = { user, cards, collections, profile }
