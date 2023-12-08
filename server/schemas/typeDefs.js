2220fffb0a744641d201a4105985046370317960
Anthony Ciccone <cicconeworldwide@gmail.com>
2023-12-07 19:02:45 -0700

    edtied models and schemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,11 +6,12 @@ const typeDefs = `
   }
 
   type Profile {
-    user_id: ID
+    UserInfo_id: ID
     username: String
     collection_id: Int
     deck_id: Int
   }
+
 
   type UserInfo {
     _id: ID
@@ -19,12 +20,6 @@ const typeDefs = `
     password: String
   }
 
-  type User {
-    _id: ID
-    firstName: String
-    lastName: String
-    email: String
-  }
 
   type Card {
     _id: ID
@@ -41,22 +36,20 @@ const typeDefs = `
 
   type Deck {
     _id: ID
-    user_id: ID
+    UserInfo_id: ID
     card_id: Int
   }
 
   type Query {
     Collections: [Collection]
     Profile: Profile
-    User(id: ID): User
     UserInfo(id: ID): UserInfo 
   }
 
   type Mutation {
-    addUser(username: String, email: String): User
-    addCollection(user_id: ID, card_id: Int): Collection
+    addCollection(UserInfo_id: ID, card_id: Int): Collection
     addCard(card_name: String, card_set: String, card_type: String, card_subtype: String, card_mana: String, card_rarity: String, card_pt: String, card_pic: String, card_artist: String): Card
-    addDeck(user_id: ID, card_id: Int): Deck
+    addDeck(UserInfo_id: ID, card_id: Int): Deck
   }
 `;
 
