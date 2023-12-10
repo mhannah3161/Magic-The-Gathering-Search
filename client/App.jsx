90edebcb7aa473e3057e89a8f544ba38e29ef3ec
Anthony Ciccone <cicconeworldwide@gmail.com>
2023-12-10 11:12:29 -0700

    updated routes for login functionality and updated app.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,29 +1,63 @@
-import { Outlet } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
-import Header from './src/components/Header.jsx';
-import Navbar from './src/components/Navbar.jsx';
-import Footer from './src/components/Footer.jsx';
-import BGimg from './src/pics/forest_bg.png';
-import themes from './src/utils/schema.json';
-
+import { Outlet, Route, Routes, Link, useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import HomePage from "./src/pages/HomePage.jsx";
+import Header from "./src/components/Header.jsx";
+import Navbar from "./src/components/Navbar.jsx";
+import Footer from "./src/components/Footer.jsx";
+import BGimg from "./src/pics/forest_bg.png";
+import Login from "./src/pages/Login.jsx";
+import Signup from "./src/pages/Signup.jsx";
+import themes from "./src/utils/schema.json";
 
 const App = () => {
-    
-    const [selectedTheme, setSelectedTheme] = useState(themes["devoid"]);
+  const [selectedTheme, setSelectedTheme] = useState(themes["devoid"]);
+  const location = useLocation();
 
-
-    return (
-<>
-<div style={{ backgroundImage: `url(${BGimg})`,
-backgroundSize: 'cover' }}>
-    <Navbar selectedTheme={selectedTheme} />
-    <Header selectedTheme={selectedTheme} setSelectedTheme={setSelectedTheme} style={{fontFamily: selectedTheme.font}}/>
-    <Outlet />
-    <Footer selectedTheme={selectedTheme} />
-    </div>
-</>
-
-    )
+  return (
+    <>
+      <div
+        style={{ backgroundImage: `url(${BGimg})`, backgroundSize: "cover" }}
+      >
+        <Navbar selectedTheme={selectedTheme} />
+        <Header
+          selectedTheme={selectedTheme}
+          setSelectedTheme={setSelectedTheme}
+          style={{ fontFamily: selectedTheme.font }}
+        />
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              <>
+                <Login />
+                <Outlet />
+              </>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <>
+                <Signup />
+                <Outlet />
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                <HomePage />
+                {location.pathname !== "/login" && (
+                  <Footer selectedTheme={selectedTheme} />
+                )}
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </>
+  );
 };
 
 export default App;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
 
 export const Signup = () => {
     const [password, setPassword] = useState("");
@@ -25,52 +26,53 @@ export const Signup = () => {
         if (password.length < 7) {
             setPasswordError("Password must be at least 7 characters");
         }
+    };
 
-    }
-
-    <div>
-        <div className="form-wrapper">
-            <div>
-            <input
-                value={username}
-                placeholder="Username"
-                onClick={e => setUsername(e.target.value)}
-                size="large"
-                />
+    return (
+        <div>
+            <div className="form-wrapper">
+                <div>
+                    <input
+                        // value={username}
+                        placeholder="Username"
+                        onChange={e => setUsername(e.target.value)}
+                        size="large"
+                    />
+                </div>
+                <div>
+                    <input
+                        value={password}
+                        placeholder="Password"
+                        onChange={e => setPassword(e.target.value)}
+                        size="large"
+                        type="password"
+                    />
+                    <label>{passwordError}</label>
+                </div>
+                <div>
+                    <input
+                        value={email}
+                        placeholder="Email"
+                        onChange={e => setEmail(e.target.value)}
+                        size="large"
+                        type="email"
+                    />
+                    <label>{emailError}</label>
+                </div>
             </div>
-            <div>
-            <input
-                value={password}
-                placeholder="Password"
-                onClick={e => setPassword(e.target.value)}
-                size="large"
-                type="password"
-            />
-            <label>{passwordError}</label>
+            <div className="button-wrapper">
+                <Button
+                    onClick={onButtonClick}
+                    type="primary"
+                    size="large"
+                >
+                    Create Login
+                </Button>
             </div>
-            <div>
-            <input
-                value={email}
-                placeholder="Email"
-                onClick={e => setEmail(e.target.value)}
-                size="large"
-                type="email"
-                />
-                <label>{emailError}</label>
-            </div>
+            <br />
+            <p>Already have an account?<Link to="/Login">Login!</Link></p>
         </div>
-        <div className="button-wrapper">
-            <Button
-                onClick={onButtonClick}
-                type="primary"
-                size="large"
-            >
-                Create Login
-            </Button>
-        </div>
-        <br/>
-        <p>Already have an account?<Link to="/Login">Login!</Link></p>
-    </div>
+    );
 }
 
 export default Signup;
